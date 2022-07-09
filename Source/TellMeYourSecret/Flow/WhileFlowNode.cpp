107fdb2f042c630a0635c7979af3031b458a82e6
Mephiztopheles <m.ahrweiler@acinex.com>
2022-07-09 22:14:01 +0200

    Update Relax at the Pool
    The ids are set correct so vanessa can take a seat

---
+++ Source/TellMeYourSecret/Flow/WhileFlowNode.cpp
@@ -0,0 +1,30 @@
+﻿#include "WhileFlowNode.h"
+
+UWhileFlowNode::UWhileFlowNode(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Misc");
+#endif
+
+	OutputPins.Add({TEXT("Break"), TEXT("Stop repetition and call Out on next cycle")});
+
+	OutputPins.Empty();
+	OutputPins.Add({TEXT("Do"), TEXT("Repeatedly called whenever start it Called")});
+	OutputPins.Add({TEXT("Out"), TEXT("Called once Do was called after Break was called")});
+}
+
+void UWhileFlowNode::ExecuteInput(const FName& PinName)
+{	
+	if (PinName == TEXT("Break"))
+	{
+		bBreak = true;
+	}
+	else if (bBreak)
+	{
+		TriggerOutput(TEXT("Out"), true);
+	}
+	else
+	{
+		TriggerOutput(TEXT("Do"));
+	}
+}
---
+++ Source/TellMeYourSecret/Flow/WhileFlowNode.h
@@ -0,0 +1,24 @@
+﻿// Copyright Acinex Games 2022
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "WhileFlowNode.generated.h"
+
+/**
+ * Can repeat his Do pin until it's stopped with a call to Break 
+ */
+UCLASS(meta=(DisplayName="Do While"))
+class TELLMEYOURSECRET_API UWhileFlowNode : public UFlowNode
+{
+	GENERATED_BODY()
+public:
+	explicit UWhileFlowNode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void ExecuteInput(const FName& PinName) override;
+
+private:
+	bool bBreak;
+};
