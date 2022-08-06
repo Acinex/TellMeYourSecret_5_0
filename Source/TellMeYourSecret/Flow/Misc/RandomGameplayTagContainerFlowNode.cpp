c7a7e5ddbd95173abd255fd291dcd399db606d47
Mephiztopheles <m.ahrweiler@acinex.com>
2022-08-06 16:32:31 +0200

    Add RandomGameplayTagContainerFlowNode

---
+++ Source/TellMeYourSecret/Flow/Misc/RandomGameplayTagContainerFlowNode.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright Acinex Games 2020
+
+
+#include "RandomGameplayTagContainerFlowNode.h"
+
+URandomGameplayTagContainerFlowNode::URandomGameplayTagContainerFlowNode(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Misc");
+#endif
+}
+
+void URandomGameplayTagContainerFlowNode::ExecuteInput(const FName& PinName)
+{
+	Selected = Tags[FMath::RandRange(0, Tags.Num() - 1)];
+
+	AddStatusReport(TEXT("Selected ") + Selected.ToString());
+
+	TriggerFirstOutput(true);
+}
+
+#if WITH_EDITOR
+bool URandomGameplayTagContainerFlowNode::IsParametersValid() const
+{
+	return Tags.Num() > 0;
+}
+#endif
---
+++ Source/TellMeYourSecret/Flow/Misc/RandomGameplayTagContainerFlowNode.h
@@ -0,0 +1,31 @@
+﻿// Copyright Acinex Games 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../StatusReportFlowNode.h"
+#include "RandomGameplayTagContainerFlowNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(NotBlueprintable, meta=(DisplayName="Select Random GameplayTagContainer"))
+class TELLMEYOURSECRET_API URandomGameplayTagContainerFlowNode : public UStatusReportFlowNode
+{
+	GENERATED_BODY()
+
+public:
+	explicit URandomGameplayTagContainerFlowNode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	virtual void ExecuteInput(const FName& PinName) override;
+
+#if WITH_EDITOR
+	virtual bool IsParametersValid() const override;
+#endif
+	
+protected:
+	UPROPERTY(EditAnywhere)
+	TArray<FGameplayTagContainer> Tags;
+
+	UPROPERTY(BlueprintReadOnly, meta=(FlowOutput))
+	FGameplayTagContainer Selected;
+};
