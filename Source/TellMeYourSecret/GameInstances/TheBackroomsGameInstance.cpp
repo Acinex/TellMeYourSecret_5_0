ea403e3a50b0a71345dad0e691ee83a9c73fa895
Mephiztopheles <m.ahrweiler@acinex.com>
2023-01-14 12:21:20 +0100

    Optimize Navigation

---
+++ Source/TellMeYourSecret/GameInstances/TheBackroomsGameInstance.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TheBackroomsGameInstance.h"
+#include "FlowAsset.h"
+#include "FlowSubSystem.h"
+
+void UTheBackroomsGameInstance::LoadRootFlow(UFlowAsset* Asset)
+{
+	UFlowSubsystem* SubSystem = GetSubsystem<UFlowSubsystem>();
+	SubSystem->LoadRootFlow(this, Asset, Asset->GetName());
+}
---
+++ Source/TellMeYourSecret/GameInstances/TheBackroomsGameInstance.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TheBackroomsGameInstance.generated.h"
+
+class UFlowAsset;
+
+/**
+ * 
+ */
+UCLASS()
+class THEBACKROOMSLT_API UTheBackroomsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void LoadRootFlow(UFlowAsset* Asset);
+};
